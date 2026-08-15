71726dc822910ac5e2d467cafb108a8a3b631e71
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-08-15 12:08:00 +0530

    modified:   client/components/logo.tsx

--- client/components/logo.tsx
+++ client/components/logo.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
     <div className="flex items-center">
-      <Image src="/light_logo.svg" alt="Logo" width={260} height={200} />
+      <Link href="/">
+        <Image
+          src="/light_logo.svg"
+          alt="Logo"
+          width={260}
+          height={200}
+        />
+      </Link>
     </div>
   );
 }
